4c1edb19008804c0596d58f73693943290cf4897
nadeemimani <108974396+nadeemimani@users.noreply.github.com>
2023-03-31 18:05:59 -0400

    s

--- scripts.js
+++ scripts.js
@@ -4,6 +4,13 @@ let names = [];
 let maxQuantities = {};
 let submittedNames = [];
 
+function capitalizeWords(str) {
+  return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
 // Fetch names and max quantities from the text file
 fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQxIo5cPZffEUpclFdDS_Whdr47iQKanqBg_tgFAHrgMerLgfyc_kqTvkT96zNl0Q497PAL1t00Tjsz/pub?gid=1685786161&single=true&output=csv')
   .then(response => response.text())
@@ -64,41 +71,33 @@ window.addEventListener("load", function () {
     const data = new FormData(form);
     const action = e.target.action;
     const name = form.name.value.trim().toLowerCase();
+    const capitalizedName = capitalizeWords(name);
     const numberOfGuests = form.quantity.value;
 
     // Check for duplicates before submitting
     if (submittedNames.includes(name)) {
-      if (numberOfGuests == 1) {
-        alert(`Looks like we’ve already received a submission for your group for ${numberOfGuests} guest. If there’s a mistake, please call or text one of us.`);
-      } else {
-        alert(`Looks like we’ve already received a submission for your group for ${numberOfGuests} guests. If there’s a mistake, please call or text one of us.`);
-      }
+      alert('You have already submitted. Please do not submit again.');
     } else {
       fetch(action, {
         method: 'POST',
         body: data,
       })
-        .then(() => {
-          if (numberOfGuests == 1) {
-            alert(`Thank you ${name} for RSVPing ${numberOfGuests} guest. See you soon!`);
-          } else {
-            alert(`Thank you ${name} for RSVPing ${numberOfGuests} guests. See you soon!`);
-          }
-          // Add the submitted name to the list of submitted names
-          submittedNames.push(name);
-        })
-        .catch(error => {
-          console.error('Error submitting the form:', error);
-        });
-    }
-  });
+      .then(() => {
+        if (numberOfGuests == 1) {
+          alert(`Thank you ${capitalizedName} for RSVPing ${numberOfGuests} guest. See you soon!`);
+        } else {
+          alert(`Thank you ${capitalizedName} for RSVPing ${numberOfGuests} guests. See you soon!`);
+        }
+      });
+  }
+});
 });
 
 function checkForm(form) {
-  //
-  // validate form fields
-  //
+//
+// validate form fields
+//
 
-  form.myButton.disabled = true;
-  return true;
+form.myButton.disabled = true;
+return true;
 }
