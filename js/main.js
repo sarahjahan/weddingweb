4ae55701634e78c62ed52aa29a93c1cb044b36e7
nadeemimani <108974396+nadeemimani@users.noreply.github.com>
2023-06-11 12:15:30 -0400

    s

--- js/main.js
+++ js/main.js
@@ -326,3 +326,97 @@ $("button").on("click", function (ev) {
 	  $('input[name="quantity"]').val("1");
 	}
   });
+
+
+  /* <----------------------- File Uploader -----------------------> */
+
+  const fs = require('fs');
+  const express = require('express');
+  const multer = require('multer');
+  const { google } = require('googleapis');
+  const readline = require('readline');
+  
+  const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+  const TOKEN_PATH = 'token.json';
+  
+  const upload = multer({ dest: 'uploads/' });
+  const app = express();
+  
+  app.post('/upload', upload.array('myFiles'), (req, res, next) => {
+	fs.readFile('credentials.json', (err, content) => {
+	  if (err) return console.log('Error loading client secret file:', err);
+	  authorize(JSON.parse(content), (auth) => uploadFiles(auth, req, res));
+	});
+  });
+  
+  app.listen(3000, () => {
+	console.log('Server started on port 3000');
+  });
+  
+  function authorize(credentials, callback) {
+	const { client_secret, client_id, redirect_uris } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+  
+	fs.readFile(TOKEN_PATH, (err, token) => {
+	  if (err) return getNewToken(oAuth2Client, callback);
+	  oAuth2Client.setCredentials(JSON.parse(token));
+	  callback(oAuth2Client);
+	});
+  }
+  
+  function getNewToken(oAuth2Client, callback) {
+	const authUrl = oAuth2Client.generateAuthUrl({
+	  access_type: 'offline',
+	  scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', (code) => {
+	  rl.close();
+	  oAuth2Client.getToken(code, (err, token) => {
+		if (err) return console.error('Error while trying to retrieve access token', err);
+		oAuth2Client.setCredentials(token);
+		fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+		  if (err) console.error(err);
+		  console.log('Token stored to', TOKEN_PATH);
+		});
+		callback(oAuth2Client);
+	  });
+	});
+  }
+  
+  function uploadFiles(auth, req, res) {
+	const drive = google.drive({ version: 'v3', auth });
+	
+	// Loop through all the uploaded files
+	req.files.forEach((file, i) => {
+	  var fileMetadata = {
+		name: file.originalname,
+	  };
+	  var media = {
+		mimeType: file.mimetype,
+		body: fs.createReadStream(file.path),
+	  };
+	  drive.files.create({
+		resource: fileMetadata,
+		media: media,
+		fields: 'id',
+	  }, function (err, file) {
+		if (err) {
+		  // Handle error
+		  console.error(err);
+		} else {
+		  console.log('File Id: ', file.id);
+		}
+		if (i === req.files.length - 1) { // If it's the last file
+		  res.send('Successfully uploaded!');
+		}
+	  });
+	});
+  }
+  
+
+
